5a0f7dadde13d62b67c0937a08885434ca3cbe54
Eunice Soh <eunice.sjy@gmail.com>
2020-07-03 13:25:25 +0800

    [EJYS] Update data to include nature reserves in parks layers

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -22,6 +22,7 @@ g_greenery = paste0(folder_data, "greenery.gpkg")
 g_planning_areas = paste0(folder_data, "planning-areas.gpkg")
 
 
+
 v_islands = st_read(g_islands)
 v_parks_nat_res = st_read(g_parks_nat_res)
 v_parks_all = st_read(g_parks_all)
@@ -31,11 +32,16 @@ v_planning_areas = st_read(g_planning_areas)
 # Rename and subset columns
 v_islands = v_islands[, c("NAME", "geom")]
 v_parks_nat_res = v_parks_nat_res[, c("NAME", "geom")]
+
+v_parks_all$habitat = ifelse(v_parks_all$TYPE == "FRINGING FOREST" & !is.na(v_parks_all$TYPE),
+                             "NATURE RESERVE (AND ADJACENT FORESTS)",
+                             as.character(v_parks_all$habitat))
 v_parks_all = v_parks_all[, c("NAME", "geom", "habitat", "AREA_SQ_M", 
                               "prop_veg_canopy_unmanaged", "prop_veg_canopy",
                               "prop_veg_no_canopy", "prop_veg",
                               "prop_veg_no_canopy_of_veg", "mangrove",
                               "veg_canopy_unmanaged")]
+
 v_greenery = v_greenery[, c("NAME", "geom", "AREA_SQ_M")]
 v_planning_areas = v_planning_areas[, c("NAME", "geom", "OBJECTID")]
 
